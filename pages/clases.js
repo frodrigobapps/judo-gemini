3bb775f10aa15582b6db7e2ac32b92b5bb6b6028
frodrigobapps <frodrigobapps@gmail.com>
2025-10-05 18:29:21 +0200

    Create clases.js

---
+++ pages/clases.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+export default function Clases() {
+  return (
+    <div className="container public-page">
+      <Head><title>Clases y Horarios | Academia de Judo</title></Head>
+      <h1>Clases y Horarios</h1>
+      <p>Ofrecemos clases para todas las edades: Infantiles, Juveniles y Adultos. ¡Contáctanos para tu primera clase de prueba!</p>
+    </div>
+  );
+}
