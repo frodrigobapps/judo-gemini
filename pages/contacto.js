260d460d0f447c6893deb9db6817847a0b5fcf6f
frodrigobapps <frodrigobapps@gmail.com>
2025-10-05 18:30:26 +0200

    Create contacto.js

---
+++ pages/contacto.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+export default function Contacto() {
+  return (
+    <div className="container public-page">
+      <Head><title>Contacto | Academia de Judo</title></Head>
+      <h1>Contáctanos</h1>
+      <p>Llámanos al 555-123-456 o visita nuestras instalaciones.</p>
+    </div>
+  );
+}
