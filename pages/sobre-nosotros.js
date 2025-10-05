fd7e8326e230710d73a42ceb47ec8540699fb263
frodrigobapps <frodrigobapps@gmail.com>
2025-10-05 18:29:47 +0200

    Create sobre-nosotros.js

---
+++ pages/sobre-nosotros.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+export default function SobreNosotros() {
+  return (
+    <div className="container public-page">
+      <Head><title>Sobre Nosotros | Academia de Judo</title></Head>
+      <h1>Nuestra Filosofía</h1>
+      <p>Más que un deporte, el Judo es una disciplina que enseña respeto, control y humildad. Conoce a nuestros Senseis.</p>
+    </div>
+  );
+}
